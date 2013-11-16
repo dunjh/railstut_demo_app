9771077ae15f72edde3a864ba225484bd4106ad2
Dun Jinhui <dunjinh@gmail.com>
2013-11-17 07:36:27 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailstutDemoApp::Application.config.secret_key_base = 'bf17d59a070cb0964625c7d041c84e3693bb4b0d8d6e3e56e5d5307fbb54239f34fa21cd103dc2586a5d1c9670ef98cda0a45c6b3dcdd6b5171aa9e031922262'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailstutDemoApp::Application.config.session_store :cookie_store, key: '_railstut_demo_app_session'
